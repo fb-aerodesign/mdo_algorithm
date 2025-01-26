13004a837101718a4941170d1b8fbc8022ff3919
Alvaro Oliveira <alvaro.oliveira@tebesensor.com>
2025-01-25 22:07:07 -0300

    refactor test script to use XfoilService for coefficient calculations and plotting

--- scripts/tests/test.py
+++ scripts/tests/test.py
@@ -2,10 +2,11 @@
 This script tests the aerodynamics module
 """
 
-from mdo_algorithm.disciplines.aerodynamics import (
-    Airfoil,
-    Wing,
-)
+from mdo_algorithm.disciplines.aerodynamics.models import Airfoil
+from mdo_algorithm.disciplines.aerodynamics.services.xfoil import XfoilService
 
 airfoil = Airfoil("s1223")
-wing = Wing(airfoil)
+
+xfoil_service = XfoilService()
+coefficients = xfoil_service.get_coefficients(airfoil, (-5, 20, 0.5), 5e5)
+xfoil_service.plot_coefficients(coefficients)
