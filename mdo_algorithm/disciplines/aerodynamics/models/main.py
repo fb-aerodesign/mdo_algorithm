9cc41b4dafc83eb0a2619d623a2e86e9e2a25dc6
AlvaroMFO <alvaromfoliveira@gmail.com>
2025-02-01 14:11:39 -0300

    Add docstrings to Airfoil and Wing dataclasses for improved documentation

--- mdo_algorithm/disciplines/aerodynamics/models/main.py
+++ mdo_algorithm/disciplines/aerodynamics/models/main.py
@@ -1,11 +1,21 @@
+"""
+Aerodynamics models
+"""
+
 from dataclasses import dataclass
 
 
 @dataclass
 class Airfoil:
+    """
+    Airfoil dataclass
+    """
     name: str
 
 
 @dataclass
 class Wing:
+    """
+    Wing dataclass
+    """
     airfoil: Airfoil
