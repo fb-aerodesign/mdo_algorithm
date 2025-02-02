2927648760048ddf829d4e922225fe105b18d4ae
Alvaro Oliveira <alvaro.oliveira@tebesensor.com>
2025-02-02 15:34:20 -0300

    Update test parameters to use Reynolds number calculation for improved accuracy

--- scripts/tests/test.py
+++ scripts/tests/test.py
@@ -4,10 +4,14 @@ This script tests the aerodynamics module
 
 from pandera.typing import DataFrame
 
+from mdo_algorithm.disciplines.common.functions import (
+    reynolds_number,
+)
+
 from mdo_algorithm.disciplines.aerodynamics.models import Airfoil
 from mdo_algorithm.disciplines.aerodynamics.services.xfoil import XfoilService, Coefficients
 
-params = [(0, 20, 0.5), 5e5, 1000]
+params = [(0, 20, 0.5), reynolds_number(12, 1, 660, 20), 1000]
 
 xfoil_service = XfoilService()
 coefficients_array: list[DataFrame[Coefficients]] = []
