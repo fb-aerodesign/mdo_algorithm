3c478dd0a5ade1aaacb908f55fb32a7d1fa32fd7
Alvaro Oliveira <alvaro.oliveira@tebesensor.com>
2025-02-02 19:21:56 -0300

    Add aerodynamics constants module for improved organization

---
+++ mdo_algorithm/disciplines/aerodynamics/constants/main.py
@@ -0,0 +1,8 @@
+"""
+Aerodynamics constants
+"""
+import os
+
+XFOIL_PATH = os.path.join("mdo_algorithm", "softwares", "xfoil")
+
+AIRFOILS_PATH = os.path.join("mdo_algorithm", "disciplines", "aerodynamics", "airfoils")
