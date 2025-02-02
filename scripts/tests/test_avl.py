70c6e7751da923078418167b4fb9568a5e4d62d4
Alvaro Oliveira <alvaro.oliveira@tebesensor.com>
2025-02-02 19:22:46 -0300

    Add test script for AVL tools functionality

---
+++ scripts/tests/test_avl.py
@@ -0,0 +1,18 @@
+"""
+This script tests de AVL tools
+"""
+
+from mdo_algorithm.disciplines.aerodynamics.models import (
+    Airfoil,
+    WingSection,
+    Wing,
+)
+
+test = Wing(
+    sections=[
+        WingSection(0, 0, 0, 0.6, 0, Airfoil("s1223")),
+        WingSection(0, 1.15, 0, 0.3, 0, Airfoil("s1223")),
+    ]
+)
+
+print(test.to_avl())
