ae67a982b4e667834fe087fe116baabba915d9d9
AlvaroMFO <alvaromfoliveira@gmail.com>
2025-02-08 12:54:47 -0300

    Add AVL service module for aerodynamic analysis and update imports

---
+++ mdo_algorithm/disciplines/aerodynamics/services/avl/main.py
@@ -0,0 +1,14 @@
+"""
+This module provides services to interact with AVL for aerodynamic analysis.
+"""
+
+
+class AvlService:
+    """
+    AVL service class
+    """
+
+    def __init__(self):
+        """
+        Initialize AVL service
+        """
