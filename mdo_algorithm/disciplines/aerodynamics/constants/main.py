47791d0559afe5f5a161d50fe3a1cb542cb311fa
AlvaroMFO <alvaromfoliveira@gmail.com>
2025-02-08 13:21:17 -0300

    Add AVL_PATH constant to aerodynamics module and restore AIRFOILS_PATH import

--- mdo_algorithm/disciplines/aerodynamics/constants/main.py
+++ mdo_algorithm/disciplines/aerodynamics/constants/main.py
@@ -5,4 +5,6 @@ import os
 
 XFOIL_PATH = os.path.join("mdo_algorithm", "softwares", "xfoil")
 
+AVL_PATH = os.path.join("mdo_algorithm", "softwares", "avl")
+
 AIRFOILS_PATH = os.path.join("mdo_algorithm", "disciplines", "aerodynamics", "airfoils")
