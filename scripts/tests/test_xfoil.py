5c08ac13dda2e221463cf5fed0d32f7d1ded039a
AlvaroMFO <alvaromfoliveira@gmail.com>
2025-02-08 12:33:38 -0300

    Refactor XFOIL test parameters to use a dictionary for improved readability and maintainability

--- scripts/tests/test_xfoil.py
+++ scripts/tests/test_xfoil.py
@@ -11,10 +11,16 @@ from mdo_algorithm.disciplines.common.functions import (
 from mdo_algorithm.disciplines.aerodynamics.models import Airfoil
 from mdo_algorithm.disciplines.aerodynamics.services.xfoil import XfoilService, Coefficients
 
-params = [(0, 20, 0.5), reynolds_number(12, 1, 660, 20), 1000]
+analysis_parameters = {
+    "alpha": (0, 20, 0.5),
+    "reynolds": reynolds_number(12, 1, 660, 20),
+    "iterations": 1000,
+}
 
 xfoil_service = XfoilService()
 coefficients_array: list[DataFrame[Coefficients]] = []
-coefficients_array.append(xfoil_service.get_coefficients(Airfoil("fx74modsm"), *params))
-coefficients_array.append(xfoil_service.get_coefficients(Airfoil("s1223"), *params))
+coefficients_array.append(
+    xfoil_service.get_coefficients(Airfoil("fx74modsm"), **analysis_parameters)
+)
+coefficients_array.append(xfoil_service.get_coefficients(Airfoil("s1223"), **analysis_parameters))
 xfoil_service.plot_coefficients(coefficients_array)
