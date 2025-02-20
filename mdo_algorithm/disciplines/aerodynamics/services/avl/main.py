d0d46ecc030ca61f89ec6f657613f6f1b484116b
AlvaroMFO <alvaromfoliveira@gmail.com>
2025-02-20 20:50:24 -0300

    Enhance AvlService to run AVL commands and retrieve aerodynamic coefficients from wing sections

--- mdo_algorithm/disciplines/aerodynamics/services/avl/main.py
+++ mdo_algorithm/disciplines/aerodynamics/services/avl/main.py
@@ -3,12 +3,26 @@ This module provides services to interact with AVL for aerodynamic analysis.
 """
 
 import os
-from typing import Literal
+import subprocess
+import re
 
-from mdo_algorithm.disciplines.aerodynamics.constants import (
-    AIRFOILS_PATH,
-)
+import numpy as np
+import pandas as pd
+from pandera.typing import DataFrame
+
+from mdo_algorithm.disciplines.common.models.geometries import Xyz
+from mdo_algorithm.disciplines.aerodynamics.constants import AVL_PATH
 from mdo_algorithm.disciplines.aerodynamics.models.geometries import Wing
+from mdo_algorithm.disciplines.aerodynamics.models.data import Coefficients
+from mdo_algorithm.disciplines.aerodynamics.models.avl import (
+    Symmetry,
+    ProfileDragSettings,
+    Section,
+    Surface,
+    Header,
+    Input,
+)
+from mdo_algorithm.disciplines.aerodynamics.functions import cl_alpha_slope
 
 
 class AvlService:
@@ -20,88 +34,144 @@ class AvlService:
         """
         Initialize AVL service
         """
+        self.__input_file_path = os.path.join(AVL_PATH, "input.avl")
+        self.__result_file_path = os.path.join(AVL_PATH, "result.txt")
 
-    def get_input_from_wing(
+    def run_avl(self, commands: list[str]) -> None:
+        """
+        Run AVL with the specified commands.
+
+        :param commands: A list of commands to pass to XFOIL.
+        :type commands: list[str]
+        """
+        if not os.path.exists(os.path.join(os.getcwd(), self.__input_file_path)):
+            raise FileNotFoundError("AVL input file not found")
+        with subprocess.Popen(
+            [os.path.join(AVL_PATH, "avl.exe"), self.__input_file_path],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+        ) as avl_process:
+            avl_process.communicate("\n".join(commands))
+
+    def get_wing_coefficients(
         self,
         wing: Wing,
-        plane_name: str = "Plane",
-        wing_name: str = "Wing",
-        mach: float = 0,
-        iysym: Literal[-1, 0, 1] = 1,
-        izsym: Literal[-1, 0, 1] = 0,
-        zsym: float = 0,
-        profile_drag_coefficient: float = 0,
-    ) -> str:
+        xfoil_coefficients_array: list[DataFrame[Coefficients]],
+        alpha: list[float] | tuple[float, float, float],
+    ) -> DataFrame[Coefficients]:
         """
-        Get AVL input from a wing object
+        Get the wing coefficients using AVL.
 
-        :param wing: Wing object
+        :param wing: The wing to analyze.
         :type wing: Wing
 
-        :param plane_name: Name of the plane
-        :type plane_name: str
+        :param xfoil_coefficients_array: A list of DataFrames containing the XFOIL coefficients for
+        each wing section.
+        :type xfoil_coefficients_array: list[DataFrame[Coefficients]]
 
-        :param wing_name: Name of the wing
-        :type wing_name: str
+        :param alpha: Angles of attack. Can be a list of angles or a tuple specifying the range
+        (start, end, increment).
+        :type alpha: list[float] | tuple[float, float, float]
 
-        :param mach: Mach number
-        :type mach: float
-
-        :param iysym: Symmetry flag along the y-axis
-        :type iysym: int
-
-        :param izsym: Symmetry flag along the z-axis
-        :type izsym: int
-
-        :param zsym: Symmetry plane position
-        :type zsym: float
-
-        :param profile_drag_coefficient: Profile drag coefficient
-        :type profile_drag_coefficient: float
-
-        :return: AVL input
-        :rtype: str
+        :return: DataFrame containing the aerodynamic coefficients.
+        :rtype: DataFrame[Coefficients]
         """
-        if len(wing.sections) < 2:
-            raise RuntimeError("The wing must have at least 2 sections")
-        sections = sorted(wing.sections, key=lambda x: x.y)
-        lines = [
-            plane_name,
-            str(round(mach, 5)),
-            f"{iysym} {izsym} {zsym}",
-            " ".join(
-                [
-                    str(round(wing.planform_area(), 3)),
-                    str(round(wing.mean_geometric_chord(), 3)),
-                    str(round(wing.span(), 3)),
-                ]
-            ),
-            " ".join(
-                [
-                    str(round(wing.mean_geometric_chord() / 4, 3)),
-                    "0",
-                    "0",
-                ]
-            ),
-            str(round(profile_drag_coefficient, 5)),
-            "SURFACE",
-            wing_name,
-        ]
-        for section in sections:
-            airfoil_path = os.path.join(AIRFOILS_PATH, f"{section.airfoil.name}.dat")
-            lines.extend(
-                [
-                    "SECTION",
-                    " ".join(
-                        [
-                            str(section.x),
-                            str(section.y),
-                            str(section.z),
-                            str(section.chord),
-                            str(section.twist),
-                        ]
-                    ),
-                    f"AFILE {airfoil_path}",
-                ]
+        if len(wing.section_array) != len(xfoil_coefficients_array):
+            raise ValueError(
+                "The number of wing sections must be equal to the number of XFOIL coefficients"
             )
-        return "\n".join(lines)
+        avl_input = Input(
+            header=Header(
+                title="Plane",
+                default_mach_number=0,
+                y_symmetry=Symmetry.IGNORE,
+                z_symmetry=Symmetry.IGNORE,
+                xy_plane_location=0,
+                reference_area=round(wing.planform_area(), 3),
+                reference_chord=round(wing.mean_aerodynamic_chord(), 3),
+                reference_span=wing.span(),
+                default_location=Xyz(0, 0, 0),
+                default_profile_drag_coefficient=None,
+            ),
+            surface_array=[
+                Surface(
+                    name="Wing",
+                    chordwise_vortice_count=12,
+                    chordwise_vortex_spacing=1,
+                    spanwise_vortice_count=20,
+                    spanwise_vortex_spacing=-1.5,
+                    mirror_surface=True,
+                    xz_plane_location=0,
+                    scale=None,
+                    translate=None,
+                    incremental_angle=None,
+                    ignore_wake=False,
+                    ignore_freestream_effect=False,
+                    ignore_load_contribution=False,
+                    profile_drag_settings=None,
+                    section_array=[
+                        Section(
+                            location=wing_section.location,
+                            chord=wing_section.chord,
+                            incremental_angle=wing_section.incremental_angle,
+                            spanwise_vortice_count=None,
+                            spanwise_vortex_spacing=None,
+                            airfoil=wing_section.airfoil,
+                            control_array=[],
+                            cl_alpha_slope_scaling=round(
+                                cl_alpha_slope(xfoil_coefficients_array[i]) / (2 * np.pi), 3
+                            ),
+                            profile_drag_settings=ProfileDragSettings.from_xfoil_coefficients(
+                                xfoil_coefficients_array[i]
+                            ),
+                        )
+                        for i, wing_section in enumerate(wing.section_array)
+                    ],
+                )
+            ],
+            body_array=[],
+        )
+        with open(os.path.join(os.getcwd(), self.__input_file_path), "w", encoding="utf-8") as f:
+            f.write(avl_input.to_input_file())
+        commands = ["OPER"]
+        append = False
+        if isinstance(alpha, tuple):
+            alpha = [float(v) for v in np.arange(alpha[0], alpha[1] + 1, alpha[2])]
+        for v in alpha:
+            commands.extend([f"A A {v}", "X", f"FT {self.__result_file_path}"])
+            if append:
+                commands.append("A")
+            append = True
+        commands.extend(["", "QUIT"])
+        self.run_avl(commands)
+        with open(os.path.join(os.getcwd(), self.__result_file_path), "r", encoding="utf-8") as f:
+            content = f.read()
+        pattern = re.compile(
+            r"Alpha\s*=\s*([-0-9.]+).*?"
+            r"CLtot\s*=\s*([-0-9.]+).*?"
+            r"CDtot\s*=\s*([-0-9.]+).*?"
+            r"Cmtot\s*=\s*([-0-9.]+)",
+            re.DOTALL,
+        )
+        data = {
+            key: pd.Series(array, dtype=float)
+            for key, array in zip(
+                Coefficients.to_schema().columns.keys(), zip(*pattern.findall(content))
+            )
+        }
+        os.remove(self.__input_file_path)
+        os.remove(self.__result_file_path)
+        df = DataFrame[Coefficients](pd.DataFrame(data))
+        df.attrs["legend"] = " | ".join(
+            [
+                "AVL",
+                "3D Wing",
+                f"Airfoil {wing.section_array[0].airfoil.name}",
+                f"S={round(wing.planform_area(), 3)}m²",
+                f"Cmac={round(wing.mean_aerodynamic_chord(), 3)}m",
+                f"B={round(wing.span(), 3)}m",
+            ]
+        )
+        return df
