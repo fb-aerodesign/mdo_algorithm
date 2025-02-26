a37c4992faecb5699808abcd529db707c67275cc
AlvaroMFO <alvaromfoliveira@gmail.com>
2025-02-26 19:44:11 -0300

    Refactor coefficient retrieval scripts to encapsulate logic in main functions for better structure and readability

--- scripts/tests/2d/coefficients.py
+++ scripts/tests/2d/coefficients.py
@@ -13,16 +13,25 @@ from mdo_algorithm.disciplines.aerodynamics.models.data import Coefficients
 from mdo_algorithm.disciplines.aerodynamics.services.xfoil import XfoilService
 from mdo_algorithm.disciplines.aerodynamics.functions import plot_coefficients
 
-analysis_parameters = {
-    "alpha": (0, 20, 0.5),
-    "reynolds": reynolds_number(12, 1, 660, 20),
-    "iterations": 1000,
-}
 
-xfoil_service = XfoilService()
-coefficients_array: list[DataFrame[Coefficients]] = []
-coefficients_array.append(
-    xfoil_service.get_coefficients(Airfoil("fx74modsm"), **analysis_parameters)
-)
-coefficients_array.append(xfoil_service.get_coefficients(Airfoil("s1223"), **analysis_parameters))
-plot_coefficients(coefficients_array)
+def main():
+    """
+    Get 2D coefficients
+    """
+    analysis_parameters = {
+        "alpha": (0, 20, 0.5),
+        "reynolds": reynolds_number(12, 1, 660, 20),
+        "iterations": 1000,
+    }
+
+    xfoil_service = XfoilService()
+    coefficients_array: list[DataFrame[Coefficients]] = []
+    coefficients_array.append(
+        xfoil_service.get_coefficients(Airfoil("fx74modsm"), **analysis_parameters)
+    )
+    coefficients_array.append(xfoil_service.get_coefficients(Airfoil("s1223"), **analysis_parameters))
+    plot_coefficients(coefficients_array)
+
+
+if __name__ == "__main__":
+    main()
--- scripts/tests/3d/coefficients.py
+++ scripts/tests/3d/coefficients.py
@@ -19,36 +19,48 @@ from mdo_algorithm.disciplines.aerodynamics.services.xfoil import XfoilService
 from mdo_algorithm.disciplines.aerodynamics.services.avl import AvlService
 from mdo_algorithm.disciplines.aerodynamics.functions import plot_coefficients
 
-wing = Wing(
-    section_array=[
-        SurfaceSection(
-            location=Xyz(0, 0, 0), chord=0.6, incremental_angle=0, airfoil=Airfoil("s1223")
-        ),
-        SurfaceSection(
-            location=Xyz(0.15, 1.15, 0), chord=0.3, incremental_angle=0, airfoil=Airfoil("s1223")
-        ),
+
+def main():
+    """
+    Get 3D coefficients
+    """
+    wing = Wing(
+        section_array=[
+            SurfaceSection(
+                location=Xyz(0, 0, 0), chord=0.6, incremental_angle=0, airfoil=Airfoil("s1223")
+            ),
+            SurfaceSection(
+                location=Xyz(0.15, 1.15, 0),
+                chord=0.3,
+                incremental_angle=0,
+                airfoil=Airfoil("s1223"),
+            ),
+        ]
+    )
+
+    alpha = (-5, 20, 0.5)
+
+    xfoil_service = XfoilService()
+    coefficients_array: list[DataFrame[Coefficients]] = [
+        xfoil_service.get_coefficients(
+            section.airfoil,
+            **{
+                "alpha": alpha,
+                "reynolds": reynolds_number(12, section.chord, 660, 20),
+                "iterations": 1000,
+            },
+        )
+        for section in wing.section_array
     ]
-)
 
-alpha = (-5, 20, 0.5)
-
-xfoil_service = XfoilService()
-coefficients_array: list[DataFrame[Coefficients]] = [
-    xfoil_service.get_coefficients(
-        section.airfoil,
-        **{
-            "alpha": alpha,
-            "reynolds": reynolds_number(12, section.chord, 660, 20),
-            "iterations": 1000,
-        },
+    avl_service = AvlService()
+    coefficients_array.append(
+        avl_service.get_wing_coefficients(
+            wing=wing, xfoil_coefficients_array=coefficients_array, alpha=alpha
+        )
     )
-    for section in wing.section_array
-]
+    plot_coefficients(coefficients_array)
 
-avl_service = AvlService()
-coefficients_array.append(
-    avl_service.get_wing_coefficients(
-        wing=wing, xfoil_coefficients_array=coefficients_array, alpha=alpha
-    )
-)
-plot_coefficients(coefficients_array)
+
+if __name__ == "__main__":
+    main()
